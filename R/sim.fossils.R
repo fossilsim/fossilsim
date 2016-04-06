2d59969dc1940cde22a426ce74395659f7299df0
Rachel Warnock <rachel.warnock@gmail.com>
2016-04-06 17:53:40 +0200

    Initial commit

---
+++ R/sim.fossils.R
@@ -0,0 +1,11 @@
+#' Simulate fossils under a uniform model of preservation
+#' @param probability of preseravtion/sampling
+#' @keywords uniform preseravtion
+#' @export
+#' @examples
+#' sim.fossils()
+#'
+
+sim.fossils<-function(){
+  print("this is a test!")
+}
