4e8431519a779d646c1ca68c1076adfd167db0b6
Rachel Warnock <rachel.warnock@gmail.com>
2016-04-07 10:32:16 +0200

    Added function for simulating uniform fossil data & documentation

--- R/sim.fossils.R
+++ R/sim.fossils.R
@@ -1,11 +1,83 @@
 #' Simulate fossils under a uniform model of preservation
-#' @param probability of preseravtion/sampling
+#' @param tree Phylo object
+#' @param basin.age Maximum age fossils may sampled
+#' @param strata Number of stratigraphic horizons
+#' @param sampling Probability of sampling/preservation
 #' @keywords uniform preseravtion
 #' @export
 #' @examples
-#' sim.fossils()
+#' t<-rtree(4)
+#' sim.fossils.unif(t,1,5,0.5)
 #'
+sim.fossils.unif<-function(tree,basin.age,strata,sampling){
+  tree<-tree
+  basin.age<-basin.age
+  strata<-strata
+  sampling<-sampling
 
-sim.fossils<-function(){
-  print("this is a test!")
+  s1=basin.age/strata # horizon length (= max age of youngest horizon)
+  horizons<-seq(s1, basin.age, length=strata)
+
+  node.ages<-n.ages(tree)
+
+  fossils<-data.frame(h=numeric(),sp=numeric())
+
+  for(h in horizons){
+
+    h.min<-h-s1
+    h.max<-h
+
+    for (i in tree$edge[,2]){ # internal nodes + tips
+
+      # work out the max age of the lineage (e.g. when that lineage became extant)
+      # & get ancestor
+      row=which(tree$edge[,2]==i)
+      ancestor=tree$edge[,1][row]
+
+      # get the age of the ancestor
+      a=which(names(node.ages)==ancestor)
+      lineage.start=node.ages[[a]]
+
+      # work out the min age of the lineage (e.g. when that lineage became extinct)
+      # & get the branch length
+      b=tree$edge.length[row]
+      lineage.end=lineage.start-b # branch length
+
+      # if the lineage is extant during this horizon
+      if ( (lineage.start >= h.min) & (lineage.end <= h.max) ) {
+
+        # 1. generate a random number from the uniform distribution
+        random.number=runif(1)
+
+        # 2. calculate the proportion of time during each horizon the lineage is extant
+        # if lineage is extant for the entire duration of the horizon
+        if ( (lineage.end <= h.min) & (lineage.start >= h.max) ) {
+          pr=1
+        }
+        # if lineage goes extinct within the horizon
+        else if (lineage.end >= h.min) {
+          pr=h.max-lineage.end
+          pr=pr/s1
+        }
+        # if lineage originates within the horizon
+        else {
+          pr=lineage.start-h.min
+          pr=pr/s1
+        }
+
+        # 3. define the probabilty
+        pr = pr * sampling
+
+        # 4. if random.number < pr { record fossil as collected }
+        if (random.number <= pr ) {
+          fossils<-rbind(fossils,data.frame(h=h,sp=i))
+        }
+      }
+
+    }
+  }
+
+  return(fossils) # in this data frame h=horizon and sp=lineage
+
+  # EOF
 }
