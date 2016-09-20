1213c06d765a28bcb3caa3bc74e83168e6f56838
Rachel Warnock <rachel.warnock@gmail.com>
2016-09-20 11:26:04 +0200

    Updating documentation

--- R/sim.fossils.R
+++ R/sim.fossils.R
@@ -178,4 +178,92 @@ sim.water.depth<-function(strata,depth=2,cycles=2) {
   # EOF
 }
 
-
+# Part 2. simulate non-uniform occurrences
+# note that this function returns an error if profile=profile (don't really know what to do about this)
+# sim.non.unif<-function(tree,basin.age,strata,PA=.5,PD=.5,DT=.5,profile) {
+#
+#   s1=basin.age/strata # max age of youngest horizon/horizon length
+#   horizons<-seq(s1, basin.age, length=strata)
+#
+#   # calculate node ages
+#   node.ages<-n.ages(tree)
+#
+#   fossils<-data.frame(x=numeric(),y=numeric())
+#
+#   depth.counter=0
+#
+#   for(h in horizons){
+#
+#     # define the water depth
+#     depth.counter=depth.counter+1
+#     current.depth=profile$y[depth.counter]
+#     #print(current.depth)
+#
+#     h.min<-h-s1
+#     h.max<-h
+#
+#     #cat("Horizon max:",h.max,"\n") # delete
+#     #cat("Horizon min:",h.min,"\n") # delete
+#
+#     for (i in tree$edge[,2]){ # internal nodes + tips
+#
+#       #print(i) # delete
+#
+#       # work out the max age of the lineage (e.g. when that lineage became extant)
+#       # & get ancestor
+#       row=which(tree$edge[,2]==i)
+#       ancestor=tree$edge[,1][row]
+#
+#       # get the age of the ancestor
+#       a=which(names(node.ages)==ancestor)
+#       lineage.start=node.ages[[a]]
+#
+#       # work out the min age of the lineage (e.g. when that lineage became extinct)
+#       # & get the branch length
+#       b=tree$edge.length[row]
+#       lineage.end=lineage.start-b # branch length
+#
+#       #cat("lineage.start:",lineage.start,"\n") # delete
+#       #cat("lineage.end:",lineage.end,"\n") # delete
+#
+#       # if the lineage is extant during this horizon
+#       if ( (lineage.start >= h.min) & (lineage.end <= h.max) ) {
+#
+#         # 1. generate a random number from the uniform distribution
+#         random.number=runif(1)
+#
+#         # 2. calculate the proportion of time during each horizon the lineage is extant
+#         # if lineage is extant for the entire duration of the horizon
+#         if ( (lineage.end <= h.min) & (lineage.start >= h.max) ) {
+#           pr=1
+#         }
+#         # if lineage goes extinct within the horizon
+#         else if (lineage.end >= h.min) {
+#           pr=h.max-lineage.end
+#           pr=pr/s1
+#         }
+#         # if lineage originates within the horizon
+#         else {
+#           pr=lineage.start-h.min
+#           pr=pr/s1
+#         }
+#
+#         # 3. define the probabilty
+#         pr = pr * PA * exp( (-(current.depth-PD)**2) / (2 * (DT ** 2)) )
+#         # the equivalent command in perl
+#         # calculate the probability of collection
+#         # my $p_collection = $PA * exp( (-($current_waterdepth-$PD)**2) / (2 * ($DT ** 2)) );
+#
+#         # 4. if random.number < pr { record fossil as collected }
+#         if (random.number <= pr ) {
+#           fossils<-rbind(fossils,data.frame(x=h,y=i))
+#         }
+#       }
+#
+#     }
+#   }
+#
+#   return(fossils) # in this data frame x=horizon and y=lineage
+#
+#   # EOF
+# }
