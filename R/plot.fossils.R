286a42514973836529a8d3265dc5d7c4f5388d4f
Rachel Warnock <rachel.warnock@gmail.com>
2016-12-11 14:55:32 +0100

    Adding plot.fossils fxn

---
+++ R/plot.fossils.R
@@ -0,0 +1,238 @@
+#' Plot simulated fossils
+#'
+#' @param tree Phylo object.
+#' @param root.edge Boolean indicating tree inclues a root edge.
+#' @param add.fossils Boolean indicating plot should inclue fossils.
+#' @param fossils Dataframe of sampled fossils (sp = edge labels. h = ages.)
+#' @examples
+#' t<-ape::rtree(10)
+#' f<-sim.fossils.poisson(t,3)
+#' @export
+draw.fossils<-function (x, fossils, root.edge = FALSE, show.tip.label = TRUE, align.tip.label = FALSE, add.fossils = TRUE, ...) {
+  x<-x  # tree
+  fossils<-fossils
+  root.edge<-root.edge
+  show.tip.label<-show.tip.label
+  align.tip.label<-align.tip.label
+  add.fossils<-add.fossils
+
+  # possible options
+  show.node.label = FALSE
+  edge.color = "black"
+  edge.width = 1
+  edge.lty = 1
+  font = 3
+  cex = par("cex")
+  tip.color = "black"
+  label.offset = 0
+  underscore = FALSE
+  plot = TRUE
+  node.depth = 1
+  no.margin = FALSE
+  x.lim = NULL
+  y.lim = NULL
+  adj = NULL
+  srt = 0
+
+  # check the tree
+  Ntip <- length(x$tip.label)
+  if (Ntip < 2) {
+    warning("found less than 2 tips in the tree")
+    return(NULL)
+  }
+  if (any(tabulate(x$edge[, 1]) == 1))
+    stop("there are single (non-splitting) nodes in your tree; you may need to use collapse.singles()")
+
+  # check edge lengths != null
+  # check tree is rooted
+  # check root edge exists if show.root.edge = T
+  # check if add.fossils = TRUE, check fossils != NULL
+
+  # required C fxns
+  .nodeHeight <- function(Ntip, Nnode, edge, Nedge, yy) .C(ape::node_height,as.integer(Ntip), as.integer(Nnode), as.integer(edge[,1]), as.integer(edge[, 2]), as.integer(Nedge), as.double(yy))[[6]]
+  .nodeDepth <- function(Ntip, Nnode, edge, Nedge, node.depth) .C(ape::node_depth,as.integer(Ntip), as.integer(Nnode), as.integer(edge[,1]), as.integer(edge[, 2]), as.integer(Nedge), double(Ntip + Nnode), as.integer(node.depth))[[6]]
+  .nodeDepthEdgelength <- function(Ntip, Nnode, edge, Nedge, edge.length) .C(ape::node_depth_edgelength, as.integer(Ntip),as.integer(Nnode), as.integer(edge[, 1]), as.integer(edge[,2]), as.integer(Nedge), as.double(edge.length), double(Ntip + Nnode))[[7]]
+
+  Nedge <- dim(x$edge)[1]
+  Nnode <- x$Nnode
+  if (any(x$edge < 1) || any(x$edge > Ntip + Nnode))
+    stop("tree badly conformed; cannot plot. Check the edge matrix.")
+  ROOT <- Ntip + 1
+
+  type <- "phylogram"
+  direction <- "rightwards"
+
+  if (is.numeric(align.tip.label)) {
+    align.tip.label.lty <- align.tip.label
+    align.tip.label <- TRUE
+  } else {
+    if (align.tip.label)
+      align.tip.label.lty <- 3
+  }
+
+  phyloORclado <- TRUE # = "phylogram"
+  horizontal <- TRUE # = "rightwards"
+
+  xe <- x$edge # used in the last part of the fxn
+  yy <- numeric(Ntip + Nnode)
+  TIPS <- x$edge[x$edge[, 2] <= Ntip, 2]
+  yy[TIPS] <- 1:Ntip
+  z <- reorder(x, order = "postorder") #?
+
+  yy <- .nodeHeight(Ntip, Nnode, z$edge, Nedge, yy)
+  xx <- .nodeDepthEdgelength(Ntip, Nnode, z$edge, Nedge, z$edge.length)
+
+  if (root.edge) {
+    xx <- xx + x$root.edge
+  }
+
+  if (no.margin)
+    par(mai = rep(0, 4))
+  if (show.tip.label)
+    nchar.tip.label <- nchar(x$tip.label)
+  max.yy <- max(yy)
+  if (is.null(x.lim)) {
+    x.lim <- c(0, NA)
+    pin1 <- par("pin")[1]
+    strWi <- strwidth(x$tip.label, "inches", cex = cex)
+    xx.tips <- xx[1:Ntip] * 1.04
+    alp <- try(uniroot(function(a) max(a * xx.tips + strWi) - pin1, c(0, 1e+06))$root, silent = TRUE)
+    if (is.character(alp)) {
+      tmp <- max(xx.tips)
+      if (show.tip.label)
+        tmp <- tmp * 1.5
+    }
+    else {
+      tmp <- if (show.tip.label)
+        max(xx.tips + strWi/alp)
+      else max(xx.tips)
+    }
+    if (show.tip.label)
+      tmp <- tmp + label.offset
+    x.lim[2] <- tmp
+  }
+  else if (length(x.lim) == 1) {
+    x.lim <- c(0, x.lim)
+  }
+  if (is.null(y.lim)) {
+    y.lim <- c(1, Ntip)
+  }
+  else if (length(y.lim) == 1) {
+    y.lim <- c(0, y.lim)
+    y.lim[1] <- 1
+  }
+
+  asp = NA
+
+  # open a new plot window
+  plot.default(0, type = "n", xlim = x.lim, ylim = y.lim, xlab = "", ylab = "", axes = FALSE, asp = asp, ...)
+
+  if (plot) {
+
+    # add colored strata
+    # y-axis:
+    # rect(xleft, ybottom, xright, ytop)
+    #y.bottom = 0
+    #y.top = max(yy)+1
+    # need to work out how to define the following
+    #x.left = min(xx)-1
+    #x.right = 0
+    #rect(xleft = x.left, xright=x.right, ybottom = y.bottom, ytop = y.top, col="red",border=NA)
+
+    # plot the tree
+    ape::phylogram.plot(x$edge, Ntip, Nnode, xx, yy, horizontal, edge.color, edge.width, edge.lty)
+
+    # add an axis (if add axis == T)
+    #axis(1, col = 'grey75', line = -1)
+
+    # format the root edge
+    if (root.edge) {
+      rootcol <- if (length(edge.color) == 1)
+        edge.color
+      else "black"
+      rootw <- if (length(edge.width) == 1)
+        edge.width
+      else 1
+      rootlty <- if (length(edge.lty) == 1)
+        edge.lty
+      else 1
+
+      # plot the root edge
+      segments(0, yy[ROOT], x$root.edge, yy[ROOT], col = rootcol, lwd = rootw, lty = rootlty)
+
+    }
+
+    # format tip labels
+    if (show.tip.label) {
+
+      # x and y co-ordinates
+      adj = 0
+      MAXSTRING <- max(strwidth(x$tip.label, cex = cex))
+      loy <- 0
+      lox <- label.offset + MAXSTRING * 1.05 * adj
+
+      if (is.expression(x$tip.label))
+        underscore <- TRUE
+      if (!underscore)
+        x$tip.label <- gsub("_", " ", x$tip.label)
+      if (phyloORclado) {
+        if (align.tip.label) {
+          xx.tmp <- max(xx[1:Ntip])
+          yy.tmp <- yy[1:Ntip]
+          segments(xx[1:Ntip], yy[1:Ntip], xx.tmp, yy.tmp, lty = align.tip.label.lty)
+        }
+        else {
+          xx.tmp <- xx[1:Ntip]
+          yy.tmp <- yy[1:Ntip]
+        }
+        text(xx.tmp + lox, yy.tmp + loy, x$tip.label,adj = adj, font = font, srt = srt, cex = cex, col = tip.color)
+      }
+    }
+
+    # add node labels
+    if (show.node.label){
+      text(xx[ROOT:length(xx)] + label.offset, yy[ROOT:length(yy)],
+           x$node.label, adj = adj, font = font, srt = srt,
+           cex = cex)
+    }
+
+    # add fossils
+    points(max(xx)-fossils$h,yy[fossils$sp],col="darkorange",pch=19,cex=1.2)
+
+  }
+  L <- list(type = type, use.edge.length = TRUE,
+            node.pos = NULL, node.depth = node.depth, show.tip.label = show.tip.label,
+            show.node.label = show.node.label, font = font, cex = cex,
+            adj = adj, srt = srt, no.margin = no.margin, label.offset = label.offset,
+            x.lim = x.lim, y.lim = y.lim, direction = direction,
+            tip.color = tip.color, Ntip = Ntip, Nnode = Nnode, root.time = x$root.time,
+            align.tip.label = align.tip.label)
+  assign("last_plot.phylo", c(L, list(edge = xe, xx = xx, yy = yy)),
+         envir = ape::.PlotPhyloEnv)
+  invisible(L)
+}
+
+# add.tscale<-function(root.time){
+#   side = 1
+#   backward = TRUE
+#
+#   lastPP <- get("last_plot.phylo", envir = .PlotPhyloEnv)
+#
+#   xscale <- range(lastPP$xx)
+#   tscale <- c(0, xscale[2] - xscale[1])
+#   tscale <- tscale[2:1]
+#
+#     if (!is.null(root.time)) {
+#       tscale <- tscale + root.time
+#       if (backward)
+#         tscale <- tscale - xscale[2]
+#     }
+#     beta <- diff(xscale)/diff(tscale)
+#     alpha <- xscale[1] - beta * tscale[1]
+#     lab <- pretty(tscale)
+#     x <- beta * lab + alpha
+#     axis(side = side, at = x, labels = lab)
+#
+# }
+
+
