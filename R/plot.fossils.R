3cdb87067e225375896dc51663868790e2de162c
Rachel Warnock <rachel.warnock@gmail.com>
2017-04-20 14:30:21 -0500

    Updating plotting functions

--- R/plot.fossils.R
+++ R/plot.fossils.R
@@ -7,15 +7,19 @@
 #' @param add.tree If TRUE plot the tree  (default = TRUE).
 #' @param add.strata If TRUE plot strata  (default = FALSE).
 #' @param add.ranges If TRUE plot ranges (default = FALSE).
+#' @param add.profile If TRUE add water depth profile (default = FALSE).
+#' @param depth.profile Water depth profile (default = NULL).
 #' @param binned If TRUE fossils are plotted in the mid point of each interval.
 #' @param add.axis If TRUE plot axis (default = TRUE).
 #' @param hide.edge If TRUE hide the root edge (default = FALSE).
+#' @param fcol Color for fossil occurrences.
 #' @examples
 #' t<-ape::rtree(10)
 #' f<-sim.fossils.poisson(t,3)
 #' draw.fossils(t,f)
 #' @export
-draw.fossils<-function (x, fossils=NULL, root.edge = FALSE, show.tip.label = FALSE, align.tip.label = FALSE, add.fossils = FALSE, add.tree = TRUE, add.strata = FALSE, strata = 1, add.ranges = FALSE, binned = FALSE, add.axis = TRUE, hide.edge = FALSE, fcol = "darkorange", fcex = 1.2, edge.width = 1,...) {
+draw.fossils<-function (x, fossils=NULL, root.edge = FALSE, show.tip.label = FALSE, align.tip.label = FALSE, add.fossils = FALSE, add.tree = TRUE, add.strata = FALSE, add.profile = FALSE,
+                        strata = 1, depth.profile = NULL , add.ranges = FALSE, binned = FALSE, add.axis = TRUE, hide.edge = FALSE, fcol = "darkorange", fcex = 1.2, edge.width = 1,...) {
   x<-x  # tree
   fossils<-fossils
   root.edge<-root.edge
@@ -26,6 +30,7 @@ draw.fossils<-function (x, fossils=NULL, root.edge = FALSE, show.tip.label = FAL
   add.strata<-add.strata
   strata<-strata
   add.ranges<-add.ranges
+  add.profile<-add.profile
   binned<-binned
   add.axis<-add.axis
   hide.edge<-hide.edge
@@ -37,7 +42,7 @@ draw.fossils<-function (x, fossils=NULL, root.edge = FALSE, show.tip.label = FAL
     align.tip.label = TRUE
 
   # possible options
-  show.node.label = FALSE
+  show.node.label = FALSE # this doesn't do anything
   edge.color = "black"
   edge.lty = 1
   font = 3
@@ -66,6 +71,7 @@ draw.fossils<-function (x, fossils=NULL, root.edge = FALSE, show.tip.label = FAL
   # check tree is rooted
   # check root edge exists if show.root.edge = T
   # check if add.fossils = TRUE, check fossils != NULL
+  # check tree is binary
 
   # required C fxns
   .nodeHeight <- function(Ntip, Nnode, edge, Nedge, yy) .C(ape::node_height,as.integer(Ntip), as.integer(Nnode), as.integer(edge[,1]), as.integer(edge[, 2]), as.integer(Nedge), as.double(yy))[[6]]
@@ -105,8 +111,8 @@ draw.fossils<-function (x, fossils=NULL, root.edge = FALSE, show.tip.label = FAL
     xx <- xx + x$root.edge
   }
 
-  if (no.margin)
-    par(mai = rep(0, 4))
+  #if (no.margin)
+   # par(mai = rep(0, 4))
   if (show.tip.label)
     nchar.tip.label <- nchar(x$tip.label)
   max.yy <- max(yy)
@@ -141,10 +147,21 @@ draw.fossils<-function (x, fossils=NULL, root.edge = FALSE, show.tip.label = FAL
     y.lim[1] <- 1
   }
 
-  asp = NA
-
-  # open a new plot window
-  plot.default(0, type = "n", xlim = x.lim, ylim = y.lim, xlab = "", ylab = "", axes = FALSE, asp = asp, ...)
+  # the order in which you use plot and par here is very important
+  if(add.profile){
+    old.par = par("usr", "mar", "oma", "xpd", "mgp","fig")
+    par(mar=c(1, 3.5, 0, 0.5)) # to reduce the margins around each plot - bottom, left, top, right -- this is harder to manipulate
+    par(oma=c(2, 0, 2, 0)) # to add an outer margin to the top and bottom of the graph -- bottom, left, top, right
+    par(xpd=NA) # allow content to protrude into outer margin (and beyond)
+    par(mgp=c(1.5, .5, 0)) # to reduce the spacing between the figure plotting region and the axis labels -- axis label at 1.5 rows distance, tick labels at .5 row
+    par(fig=c(0,1,0,0.3))
+    plot.default(0, type = "n", xlim = x.lim, ylim = y.lim, xlab = "", ylab = "", axes = FALSE, asp = NA, ...)
+    par(fig=c(0,1,0.3,1))
+  }
+  else{
+    # open a new plot window
+    plot.default(0, type = "n", xlim = x.lim, ylim = y.lim, xlab = "", ylab = "", axes = FALSE, asp = NA, ...)
+  }
 
   if (plot) {
 
@@ -182,7 +199,7 @@ draw.fossils<-function (x, fossils=NULL, root.edge = FALSE, show.tip.label = FAL
       }
 
       if(add.axis)
-        axis(1, col = 'grey75', at = axis.strata, labels = FALSE, lwd = 2)
+        axis(1, col = 'grey75', at = axis.strata, labels = FALSE, lwd = 2, line = 0.5) # PROBLEM
       #axis(1, col = 'grey75', at = axis.strata, labels = seq(ba, 0, by=-s1) )
 
     }
@@ -242,7 +259,7 @@ draw.fossils<-function (x, fossils=NULL, root.edge = FALSE, show.tip.label = FAL
            cex = cex)
     }
 
-    # add fossils
+    # fossils
     if(add.fossils){
       if(binned){
         s2 = (ba / strata)/2
@@ -252,6 +269,7 @@ draw.fossils<-function (x, fossils=NULL, root.edge = FALSE, show.tip.label = FAL
         points(max(xx)-fossils$h,yy[fossils$sp],col=fcol,pch=19,cex=fcex)
     }
 
+    # ranges
     if(add.ranges){
       buffer = 0.01 * max(xx) # buffer for singletons
       s2 = 0
@@ -270,8 +288,14 @@ draw.fossils<-function (x, fossils=NULL, root.edge = FALSE, show.tip.label = FAL
       }
     }
 
-
+    # water depth profile
+    add.depth.axis = TRUE
+    if(add.profile){
+      add.depth.profile(depth.profile,axis.strata,strata,add.axis,add.depth.axis)
+      par(old.par)
+    }
   }
+
   L <- list(type = type, use.edge.length = TRUE,
             node.pos = NULL, node.depth = node.depth, show.tip.label = show.tip.label,
             show.node.label = show.node.label, font = font, cex = cex,
@@ -282,6 +306,27 @@ draw.fossils<-function (x, fossils=NULL, root.edge = FALSE, show.tip.label = FAL
   assign("last_plot.phylo", c(L, list(edge = xe, xx = xx, yy = yy)),
          envir = ape::.PlotPhyloEnv)
   invisible(L)
+}
+
+add.depth.profile<-function(depth.profile,axis.strata,strata,add.axis,add.depth.axis){
+  par(fig=c(0,1,0,0.3), new = T)
+  # change the y-axis scale for depth
+  u = par("usr") # current scale
+  tol = 0.5
+  depth = depth.profile$y
+  par(usr = c(u[1], u[2], min(depth) - tol, max(depth) + tol))
+  # define x-axis values (time)
+  time = axis.strata[1:strata] + ((axis.strata[2] - axis.strata[1])/2)
+  points(time,depth)
+  lines(x = axis.strata, y = rep(1, length(axis.strata)), col = "grey75", lwd = 2, lty = 3)
+  if(add.axis){
+    axis(1, col = 'grey75', at = axis.strata, labels = FALSE, lwd = 2)
+    mtext(1, col = 'grey75', text="Time before present", line = 1)
+  }
+  if(add.depth.axis){
+    axis(2, col = 'grey75', labels = TRUE, lwd = 2, las = 2, col.axis = 'grey75', line = 0.5)
+    mtext(2, col = 'grey75', text="Relative depth", line= 2)
+  }
 }
 
 
