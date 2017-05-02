9faa19356bbf6dba3771656195c2a9a106ca3dbd
Rachel Warnock <rachel.warnock@gmail.com>
2017-05-02 11:13:45 +0200

    Updating fossils class options and functions

--- R/fossils.R
+++ R/fossils.R
@@ -1,14 +1,29 @@
-
-# define class fossils
-fossils<-function(f, speciation.mode = "asymmetric"){
-  # do some checking: are sp and h available?
-  me <- f
-  class(me) <- c("fossils", class(me))
+#' Fossils object
+#'
+#' Create a fossil record object. The input is taken to be a dataframe or list with phylo object edge labels (sp) and sampled fossil ages (h).
+#'
+#' @param data Dataframe or list of sampled fossils (sp = edge labels, h = ages).
+#' @param ages String indicating how fossil ages are defined relative to interval ages (e.g. continuous, horizon.max)
+#' @param speciation.mode String indicating the speciation concept fossils were generated under (e.g. asymmetric/budding, symmetric/bifurcating, mixed)
+#'
+#' @export
+fossils<-function(data, ages = NULL, speciation.mode = NULL){
+  if(is.null(data$sp) || is.null(data$h))
+    stop("Edge labels and fossil ages must be specified using 'sp' and 'h'")
+  if(is.list(data))
+    data <- as.data.frame(data)
+  me <- data
+  attr(me, "class") <- c("fossils", class(me))
+  attr(me, "ages") <- ages
   attr(me, "speciation") <- speciation.mode
   return(me)
 }
 
+#' @export
+#' @aliases fossils
 print.fossils<-function(f, max.length = 10, ...){
+  if(length(f$sp) < max.length)
+    max.length = length(f$sp)
   print(as.data.frame(f)[1:max.length,])
   if(length(f$sp) > max.length)
     cat("...\n")
@@ -17,11 +32,28 @@ print.fossils<-function(f, max.length = 10, ...){
     cat("Speciation mode:", attr(f, "speciation"))
 }
 
+#' @export
+#' @aliases fossils
+summary.fossils<-function(f, max.length = 10, ...){
+  print(f, max.length = max.length)
+}
 
+#' @export
+#' @rdname  fossils
+as.fossils<-function(data, ...) UseMethod("as.fossils")
 
-# as.fossil.record<-function(f,...){
-#   if(identical(class(f),"fossil.record"))
-#     return(f)
-#   else
-#     UseMethod("as.fossil.record")
-# }
+as.fossils.default<-function(data, ...){
+  fossils(data, ...)
+}
+
+#' @export
+#' @rdname  fossils
+is.fossils<-function(fossils){
+  if(inherits(fossils, "fossils"))
+    TRUE
+  else
+    FALSE
+}
+
+# head(f)
+# tail(f)
