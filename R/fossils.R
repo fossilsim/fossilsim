2e820a97a661d8af623b0c6cd8b45e895c618758
bjoelle <joelle.barido-sottani@bsse.ethz.ch>
2017-10-31 18:09:08 +0100

    new fossils format

--- R/fossils.R
+++ R/fossils.R
@@ -1,22 +1,48 @@
 #' Fossils object
 #'
-#' Create a fossil record object. The input is taken to be a dataframe or list with phylo object edge labels (sp) and sampled fossil ages (h).
+#' Create a fossil record object from a dataframe or a list. The input is taken to be a dataframe or list.
+#' 
+#' The fossil record object contains 6 fields containing for each fossil the following data:
+#' \itemize{
+#'  \item \code{sp} the label of the corresponding species. This label matches with tip labels in the corresponding phylogeny
+#'  and with species labels in the taxonomy record.
+#'  \item \code{node} the label of the sampling node in the phylogeny, i.e the node at the end of the edge on which the fossil was sampled
+#'  \item \code{origin} the label of the node where the corresponding species originated in the phylogeny
+#'  \item \code{mode} the speciation mode that gave rise to the corresponding species. Can be one of \code{b} (budding/asymmetric), \code{s} (symmetric/bifurcation) or \code{a} (anagenic).
+#'  \item \code{hmin} the lowest bound of the time interval in which that fossil was sampled
+#'  \item \code{hmax} the highest bound of the time interval in which that fossil was sampled. Is equal to \code{hmin} if sampling times are exact.
+#' }
 #'
-#' @param data Dataframe or list of sampled fossils (sp = edge labels, h = ages).
-#' @param ages String indicating how fossil ages are defined relative to interval ages (e.g. continuous, interval.max)
-#' @param speciation.mode String indicating the speciation concept fossils were generated under (e.g. asymmetric/budding, symmetric/bifurcating, mixed)
+#' @param data Dataframe or list of sampled fossils. See Details for the list of required fields.
+#' @param speciation.mode String indicating the speciation concept fossils were generated under (e.g. asymmetric/budding, symmetric/bifurcating, mixed). Defaults to mixed.
 #'
 #' @export
-fossils<-function(data, ages = NULL, speciation.mode = NULL){
-  if(is.null(data$sp) || is.null(data$h))
-    stop("Edge labels and fossil ages must be specified using 'sp' and 'h'")
-  if(is.list(data))
-    data <- as.data.frame(data)
-  me <- data
-  attr(me, "class") <- c("fossils", class(me))
-  attr(me, "ages") <- ages
-  attr(me, "speciation") <- speciation.mode
-  return(me)
+fossils<-function(data, speciation.mode = c("asymmetric", "symmetric", "mixed")){
+  if(is.list(data)) data <- as.data.frame(data)
+  
+  # check for required fields
+  required_fields = c("origin", "sp", "node", "mode", "hmin", "hmax")
+  missing = which(! required_fields %in% colnames(data))
+  if(length(missing) > 0) stop(paste0("Missing required fields: ", paste(required_fields[missing], collapse = ", ")))
+  
+  # check for additional fields
+  additional = which(! colnames(data) %in% required_fields)
+  if(length(additional) > 0) {
+    warning(paste0("These fields will be discarded: ", paste(colnames(data)[additional], collapse = ", ")))
+    data[,additional] = NULL
+  }
+  
+  attr(data, "class") <- c("fossils", class(data))
+  
+  # handling speciation.mode attribute
+  if(length(speciation.mode) > 1) speciation.mode = "mixed"
+  if(!speciation.mode %in% c("asymmetric", "symmetric", "mixed")) {
+    warning("Unrecognized speciation mode, defaulting to mixed")
+    speciation.mode = "mixed"
+  }
+  attr(data, "speciation") <- speciation.mode
+  
+  return(data)
 }
 
 #' @export
@@ -29,7 +55,7 @@ print.fossils<-function(f, max.length = 10, ...){
     if(length(f$sp) > max.length)
       cat("...\n")
   }
-  cat("Fossil record with", length(f$h), "occurrences repesenting", length(unique(f$sp)), "species\n")
+  cat("Fossil record with", length(f$sp), "occurrences representing", length(unique(f$sp)), "species\n")
   if(!is.null(attr(f,"speciation")))
     cat("Speciation mode:", attr(f, "speciation"))
 }
