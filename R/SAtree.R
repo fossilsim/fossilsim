9460972888479244306d4ec3522609c4deb19462
bjoelle <bjoelle@student.ethz.ch>
2018-09-17 16:22:58 +0200

    fixed bug with converting to SAtree format when fossil df is empty

--- R/SAtree.R
+++ R/SAtree.R
@@ -1,7 +1,7 @@
 #' Tree with sampled ancestors represented as zero-length edges
 #'
 #' @description
-#' Converts a phylo object to SAtree.
+#' Converts a phylo object to SAtree, without modification of tip labels.
 #'
 #' @param tree Phylo object.
 #' @param complete Whether the tree is complete. Default TRUE. If the tree is not complete, then all fossil tips correspond to fossil samples, otherwise only sampled ancestors are considered samples.
@@ -16,6 +16,7 @@ SAtree = function(tree, complete = TRUE) {
 
 #' Transforms a tree and fossils dataframe to a combined SA tree.
 #' Sampled ancestors are represented as tips on zero-length edges to maintain compatibility with the ape format.
+#' Tip labels are set to "species id"_"index", where the most recent tip of a given species receives index 1 and indices increase towards the past.
 #'
 #' @param tree Phylo object.
 #' @param fossils Fossils object.
@@ -32,17 +33,20 @@ SAtree = function(tree, complete = TRUE) {
 #' plot(t2)
 #' @export
 SAtree.from.fossils = function(tree, fossils) {
-  if(length(fossils[,1])==0) return(tree)
-
+  if(length(fossils[,1])==0) {
+    tree$tip.label = paste0(tree$tip.label, "_", 1)
+    return(SAtree(tree, TRUE))
+  }
+  
   fossils$h = (fossils$hmin + fossils$hmax)/2
   fossils = fossils[order(fossils$sp, -fossils$h),]
-
+  
   ntips = length(tree$tip.label)
   totalnodes = ntips + tree$Nnode
-
+  
   depths = ape::node.depth.edgelength(tree)
   times = max(depths) - depths
-
+  
   # adding root edge in case fossils appear on it
   if(!is.null(tree$root.edge)) {
     root = (ntips + length(fossils[,1]))*2
@@ -50,14 +54,14 @@ SAtree.from.fossils = function(tree, fossils) {
     tree$edge.length = c(tree$edge.length, tree$root.edge)
     times[root] = max(times) + tree$root.edge
   }
-
+  
   #renaming all species not in fossils
   for(i in 1:ntips) {
     if(!i %in% fossils$sp) {
       tree$tip.label[i] = paste0(tree$tip.label[i], "_", 1)
     }
   }
-
+  
   current_spec = 0
   count_spec = 1
   for(i in 1:length(fossils[,1])) {
@@ -75,7 +79,7 @@ SAtree.from.fossils = function(tree, fossils) {
     times[totalnodes+1] = fossils$h[i]
     totalnodes=totalnodes+1
     tree$Nnode=tree$Nnode+1
-
+    
     #adding fossil tip
     tree$edge = rbind(tree$edge,c(totalnodes,-i))
     tree$edge.length = c(tree$edge.length,0)
@@ -84,21 +88,21 @@ SAtree.from.fossils = function(tree, fossils) {
     count_spec = count_spec +1
   }
   if(current_spec <= ntips) tree$tip.label[current_spec] = paste0(tree$tip.label[current_spec], "_", count_spec)
-
+  
   #handling root edge again, mrca may have been modified by the inclusion of fossils
   if(!is.null(tree$root.edge)) {
     rootedge = which(tree$edge[,1] == root)
     newroot = tree$edge[rootedge,2]
-
+    
     rootidx = which(tree$edge == newroot)
     tree$edge[which(tree$edge == ntips + 1)] = newroot
     tree$edge[rootidx] = ntips + 1
-
+    
     tree$root.edge = tree$edge.length[rootedge]
     tree$edge = tree$edge[-rootedge,]
     tree$edge.length = tree$edge.length[-rootedge]
   }
-
+  
   #renumbering all nodes to maintain ape format
   for(n in totalnodes:(ntips+1)) {
     tree$edge[which(tree$edge==n)] = n + length(fossils[,1])
@@ -106,10 +110,10 @@ SAtree.from.fossils = function(tree, fossils) {
   for(i in 1:length(fossils[,1])) {
     tree$edge[which(tree$edge==-i)] = ntips + i
   }
-
+  
   #force reordering for nice plotting
   attr(tree,"order")=NULL
   tree = ape::reorder.phylo(tree)
-
+  
   SAtree(tree, TRUE)
 }
